4febbf612ec327e2e56e09220a69ab50cf1dbfef
Saud <saudd2gh@gmail.com>
2019-01-10 10:33:53 +0800

    basic rectangle-point collision added

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,14 @@
 
 var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+var handle = {
+	x: 100,
+	y: 50,
+	w: 100,
+	h: 100
+};
 
 var context = canvas.getContext('2d');
 
@@ -13,9 +20,49 @@ function Rectangle(x, y, w, h){
 
 	this.draw = function(){
 		context.fillStyle = 'blue';
-		context.fillRect(x, y, w, h);
+		context.fillRect(this.x, this.y, this.w, this.h);
+	}
+
+	this.update = function(x,y){
+		context.clearRect(0, 0, width, height);
+
+		this.x = x;
+		this.y = y;
+
+		this.draw();
 	}
 }
 
-var rect = new Rectangle(100,50,300,300);
+var rect = new Rectangle(handle.x,handle.y,handle.w,handle.h);
 rect.draw();
+
+function onMouseMove(event){
+	
+	console.log(event.clientX);
+	console.log(event.clientY);
+	handle.x = event.clientX;
+	handle.y = event.clientY;
+	rect.update(handle.x, handle.y);
+}
+
+function onMouseUp(event){
+	document.body.removeEventListener('mousemove', onMouseMove);
+	document.body.removeEventListener('mouseup', onMouseUp);
+}
+
+function pointInRect(x, y, rect){
+	return inRange(x, rect.x, rect.x + rect.w) && inRange(y, rect.y, rect.y + rect.h);
+}
+
+function inRange(value, min, max){
+	return value >= min && value <= max;
+}
+
+document.body.addEventListener('mousedown', function(event){
+	if(pointInRect(event.clientX, event.clientY, handle)){
+		console.log(event.clientX);
+		console.log(event.clientY);
+		document.body.addEventListener('mousemove', onMouseMove);
+		document.body.addEventListener('mouseup', onMouseUp);
+	}
+});
