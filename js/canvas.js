71110dad83aff40f81b9e0c17d722d7658662c58
Saud <saudd2gh@gmail.com>
2019-01-10 11:05:23 +0800

    basic canvas drawn

---
+++ js/canvas.js
@@ -0,0 +1,11 @@
+
+
+function draw(){
+	var canvas = document.querySelector('canvas');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	var context = canvas.getContext('2d');
+
+}
+
+draw();
