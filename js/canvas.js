011641838be066a50ffade8effc059836fbb8a93
Saud <saudd2gh@gmail.com>
2019-01-10 10:18:51 +0800

    basic rectangle object added

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,21 @@
 
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-function draw(){
-	var canvas = document.querySelector('canvas');
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	var context = canvas.getContext('2d');
+var context = canvas.getContext('2d');
 
+function Rectangle(x, y, w, h){
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+
+	this.draw = function(){
+		context.fillStyle = 'blue';
+		context.fillRect(x, y, w, h);
+	}
 }
 
-draw();
+var rect = new Rectangle(100,50,300,300);
+rect.draw();
