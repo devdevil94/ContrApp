88f0d7a3813195dda80f32adde9c3dc54c9b362a
Saud <saudd2gh@gmail.com>
2019-01-12 19:57:08 +0400

    minor svg fixes

--- js/drawing-board.js
+++ js/drawing-board.js
@@ -12,18 +12,36 @@ function makeDraggable(event) {
 	var selectedElement = false;
 
 	function startDrag(event) {
-		if (event.target.classList.contains('draggable')) {
+		if (event.target.classList.contains('comp-group')) {
     		selectedElement = event.target;
+    		
+    		console.log(selectedElement);
+		    offset = getMousePosition(event);
+
+		    offset.x -= parseFloat(selectedElement.getAttributeNS(null, "x"));
+		    offset.y -= parseFloat(selectedElement.getAttributeNS(null, "y"));
   		}
 	}
 	function drag(event) {
 		if (selectedElement) {
 		    event.preventDefault();
-		    var x = parseFloat(selectedElement.getAttributeNS(null, "x"));
-		    selectedElement.setAttributeNS(null, "x", x + 0.1);
+
+		    var coord = getMousePosition(event);
+
+		    selectedElement.setAttributeNS(null, "x", coord.x - offset.x);
+    		selectedElement.setAttributeNS(null, "y", coord.y - offset.y);
   		}
 	}
 	function endDrag(event) {
 		selectedElement = null;
+	}
+
+	function getMousePosition(evt) {
+  		var CTM = svg.getScreenCTM();
+
+		return {
+			x: (evt.clientX - CTM.e) / CTM.a,
+			y: (evt.clientY - CTM.f) / CTM.d
+		};
 	}
 }
