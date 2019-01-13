bd7ff91cee6163c21f922cb88d2614fe2df43068
Saud <saudd2gh@gmail.com>
2019-01-13 09:08:18 +0400

    Dragging capability fixed

--- js/drawing-board.js
+++ js/drawing-board.js
@@ -1,7 +1,6 @@
 
 
 var svg = document.querySelector('svg');
-svg.addEventListener('click', getPos);
 
 function getMousePosition(event) {
 	var CTM = svg.getScreenCTM();
@@ -12,14 +11,10 @@ function getMousePosition(event) {
 	};
 }
 
-function getPos(event){
-	console.log(getMousePosition(event).x + " " + getMousePosition(event).y + "target = " + event.target);
-}
 
 var rect1 = new Rectangle(80,50,100,100);
 rect1.draw(svg);
-rect1.addEventListeners();
 
 var rect2 = new Rectangle(150,250,100,100);
 rect2.draw(svg);
-rect2.addEventListeners();
+
--- js/rectangle.js
+++ js/rectangle.js
@@ -8,8 +8,9 @@ class Rectangle{
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		this.rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
 		this.offset= 0;
+		this.dragging = false;
+		this.rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
 	}
 //<text x="80" y="78" fill="black">Hello World</text>
 	draw(svg){
@@ -21,39 +22,47 @@ class Rectangle{
 		this.rect.setAttributeNS(null, 'height', this.h);
 		this.rect.setAttributeNS(null, 'fill', '#007bff');
 
-		svg.appendChild(this.rect);
-	}
-
-	addEventListeners(){
 		this.rect.addEventListener('mousedown', this.startDrag);
 		this.rect.addEventListener('mousemove', this.drag);
 		this.rect.addEventListener('mouseup', this.endDrag);
 		this.rect.addEventListener('mouseleave', this.endDrag);
+
+		svg.appendChild(this.rect);
 	}
 
 	startDrag(event) {
+		if(!this.dragging){
+		    this.offset = getMousePosition(event);
 
-		// console.log(event.target);
+		    console.log('startDrag : x = ' + this.offset.x + ' y = ' + this.offset.y);
 
-	 //    this.offset = getMousePosition(event.target);
+		    this.offset.x -= parseFloat(event.target.getAttributeNS(null, "x"));
+		    this.offset.y -= parseFloat(event.target.getAttributeNS(null, "y"));
+		    console.log('startDrag : x = ' + this.offset.x + ' y = ' + this.offset.y);
 
-	 //    this.offset.x -= parseFloat(event.target.getAttributeNS(null, "x"));
-	 //    this.offset.y -= parseFloat(event.target.getAttributeNS(null, "y"));
+		    this.dragging = true;
+		}
+	 //    event.target.addEventListener('mousemove', this.drag);
+	 //    event.target.addEventListener('mouseup', this.endDrag);
+		// event.target.addEventListener('mouseleave', this.endDrag);
 	}
 
 	drag(event) {
-	// 	if (event.target.classList.contains('draggable')) {
-	// 	    event.preventDefault();
+		if(this.dragging){
+			console.log(event.target);
+		    event.preventDefault();
 
-	// 	    var coord = getMousePosition(event.target);
+		    var coord = getMousePosition(event);
 
-	// 	    event.target.setAttributeNS(null, "x", coord.x - this.offset.x);
- //    		event.target.setAttributeNS(null, "y", coord.y - this.offset.y);
- //  		}
-	 }
+		    console.log('drag x = ' + this.offset.x + ' y = ' + this.offset.y);
 
-	endDrag(event) {
-		// event.target = null;
+		    event.target.setAttributeNS(null, "x", coord.x - this.offset.x);
+			event.target.setAttributeNS(null, "y", coord.y - this.offset.y);
+		}
 	}
 
+	endDrag(event) {
+		console.log('endDrag');
+		this.dragging = false;
+	}
 }
