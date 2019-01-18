66ea4021265a96a3c77c0dbd89d83cac4f053949
Saud <saudd2gh@gmail.com>
2019-01-18 19:08:09 +0400

    Drawing simple line test

--- js/line.js
+++ js/line.js
@@ -2,5 +2,19 @@
 
 class Line{
 	
-	
+	constructor(startPoint,endPoint){
+		this.startX = startPoint.x;
+		this.startY = startPoint.y;
+		this.endX = endPoint.x;
+		this.endY = endPoint.y;
+	}
+
+	draw(svg){
+		var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+		path.setAttributeNS(null, 'd', 'M' + this.startX + ',' + this.startY + 'L' + this.endX + ',' + this.endY);
+		path.setAttributeNS(null, 'stroke', 'black');
+		path.setAttributeNS(null, 'stroke-width', 3);
+
+		svg.appendChild(path);
+	}
 }
--- js/main.js
+++ js/main.js
@@ -1,42 +1,68 @@
 
 
 var svg = new SVG(SVG_WIDTH, SVG_HEIGHT).getSVG();
-var outputDiv = document.getElementById('output');
 
-
-var plant1 = new Plant(80,50);
-plant1.create(svg);
-plant1.setTransferFunction('1');
-plant1.determineOutput();
-
-var out = plant1.getOutput();
-
-var editButton = document.getElementById('edit');
-var tFunctionInput = document.getElementById('tFunction');
-
-changeOutputDiv();
-
-editButton.addEventListener('click', function(){
-	var tFunctionValue = tFunctionInput.value;
-
-	plant1.setTransferFunction(tFunctionValue);
-	plant1.determineOutput();
-	out = plant1.getOutput();
-
-	changeOutputDiv();
-
-});
-
-function changeOutputDiv(){
-	var img = document.createElement('img');
-	img.src = 'http://latex.codecogs.com/svg.latex?' + math.parse(out).toTex();
-
-	if(outputDiv.childNodes.length > 1){
-		console.log(outputDiv.childNodes);
-		outputDiv.removeChild(outputDiv.childNodes[2]);
-	}
-	outputDiv.appendChild(img);
+var start = {
+	x: 50,
+	y: 180
 }
+var end = {
+	x: 150,
+	y: 60
+}
+
+var line = new Line(start, end);
+line.draw(svg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var plant1 = new Plant(80,50);
+// plant1.create(svg);
+// plant1.setTransferFunction('1');
+// plant1.determineOutput();
+// var out = plant1.getOutput();
+
+// var outputDiv = document.getElementById('output');
+// var editButton = document.getElementById('edit');
+// var tFunctionInput = document.getElementById('tFunction');
+
+// changeOutputDiv();
+
+// editButton.addEventListener('click', function(){
+// 	var tFunctionValue = tFunctionInput.value;
+
+// 	plant1.setTransferFunction(tFunctionValue);
+// 	plant1.determineOutput();
+// 	out = plant1.getOutput();
+
+// 	changeOutputDiv();
+
+// });
+
+// function changeOutputDiv(){
+// 	var img = document.createElement('img');
+// 	img.src = 'http://latex.codecogs.com/svg.latex?' + math.parse(out).toTex();
+
+// 	if(outputDiv.childNodes.length > 1){
+// 		console.log(outputDiv.childNodes);
+// 		outputDiv.removeChild(outputDiv.childNodes[2]);
+// 	}
+// 	outputDiv.appendChild(img);
+// }
 
 
 // svg.addEventListener('click', function(event){
