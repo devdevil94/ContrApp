236d694717e6b8e577faa6dcb07d33e82edd3e49
Saud <saudd2gh@gmail.com>
2019-01-24 20:22:20 +0400

    minor fixes

--- js/multiplier.js
+++ js/multiplier.js
@@ -3,8 +3,8 @@
 class Multiplier{
 	
 	constructor(){
-		this.input1 = 't+3';
-		this.input2 = '3*t+t^2';
+		this.input1 = '(t+3)';
+		this.input2 = '(3*t+t^2)';
 		this.output = '';
 	}
 
@@ -14,7 +14,7 @@ class Multiplier{
 	determineOutput(){
 		//var parsedInput1 = math.parse(this.input1);
 		//var parsedInput2 = math.parse(this.input2);
-		this.output = this.input1 + '*' + this.input2;
+		this.output = math.multiply(this.input1, this.input2);
 		//var parsedOutput = math.parse(this.output);
 		return math.simplify(this.output).toString();
 	}
