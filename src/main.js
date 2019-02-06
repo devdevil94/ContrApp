1e7325ff4d6c5ecc5f7a6c6b37f72712f398f070
Saud <saudd2gh@gmail.com>
2019-02-06 11:42:15 +0400

    bootstrap, mathjs and algebrite installed

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import { constants } from '../dist/js/constants.js';
 
 //Important functions/objects to call
 var svg = new SVG(constants.SVG_WIDTH, constants.SVG_HEIGHT).getSVG();
-/////////////////////////////////////
+//////////////////////////////////////
 
 
 var block = new Block(constants.BLOCK_WIDTH, constants.BLOCK_HEIGHT);
