7114fe1309b5426f1c96e1da607ff933b56525e5
Saud <saudd2gh@gmail.com>
2019-02-08 17:07:21 +0400

    multiplier class testing

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import SVG from '../dist/js/svg';
 import Block from '../dist/js/block';
 import {constants, Utils} from '../dist/js/utils';
 import 'bootstrap/dist/css/bootstrap.css';
+import Multiplier from '../dist/js/multiplier';
 
 require('./style.css');
 require('./index.html');
@@ -13,9 +14,12 @@ require('./index.html');
 
 
 //Important functions/objects to call
-var svg = new SVG(constants.SVG_WIDTH, constants.SVG_HEIGHT).getSVG();
+//var svg = new SVG(constants.SVG_WIDTH, constants.SVG_HEIGHT).getSVG();
 //////////////////////////////////////
 
+var mul = new Multiplier();
+mul.setInput1('(t+3)');
+mul.setInput2('1/(6*t^2+3)');
+mul.calcOutput();
 
-var block = new Block(constants.BLOCK_WIDTH, constants.BLOCK_HEIGHT);
-block.create(svg);
+console.log(mul.getOutput());
