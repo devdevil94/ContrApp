fa0010cbc3a04ceb552ffa236e3594354339e6ea
Saud <saudd2gh@gmail.com>
2019-02-08 17:03:26 +0400

    multiplier class testing

--- dist/js/multiplier.js
+++ dist/js/multiplier.js
@@ -1,17 +1,25 @@
 
-const Algebrite = require('algebrite');
+//const Algebrite = require('algebrite');
 const cq = require('coffeequate');
 
 export default class Multiplier{
 	
 	constructor(){
-		this.input1 = '(t+3)';
-		this.input2 = '(3*t+t^2)';
+		this.input1 = '';
+		this.input2 = '';
 		this.output = '';
 	}
-
-	setInput1(input1){this.input1 = input1;}
-	setInput2(input2){this.input2 = input2;}
+	setInput1(input1){
+		var input1Exp = input1.replace(/\^/g, '**');
+		this.input1 = input1Exp;
+	}
+	setInput2(input2){
+		var input2Exp = input2.replace(/\^/g, '**');
+		this.input2 = input2Exp;
+	}
 	getOutput(){return this.output;}
-	determineOutput(){this.output = cq(this.input1 + '*' + this.input2);}
+	calcOutput(){
+		var outputExp = cq(this.input1 + '*' + this.input2).simplify().toString();
+		this.output = outputExp.replace(/\*\*/g, '^');
+	}
 };
