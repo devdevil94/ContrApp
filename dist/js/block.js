325eb91700099c460ae3c7160d54d5847344bf3a
Saud <saudd2gh@gmail.com>
2019-04-18 23:24:55 +0400

    deselect component's selected circle

--- dist/js/block.js
+++ dist/js/block.js
@@ -24,14 +24,14 @@ export default class Block {
     this.container.setFunction(this.transFunction);
     this.setSelected(false);
   }
-  createPath(event) {
-    var blockCircle = event.target;
-    var x = blockCircle.getAttributeNS(null, "cx");
-    var y = blockCircle.getAttributeNS(null, "cy");
-    console.log(x + " " + y);
-    var svg = blockCircle.parentNode.parentNode;
-    var path = new Path(svg, x, y);
-  }
+  // createPath(event) {
+  //   var blockCircle = event.target;
+  //   var x = blockCircle.getAttributeNS(null, "cx");
+  //   var y = blockCircle.getAttributeNS(null, "cy");
+  //   console.log(x + " " + y);
+  //   var svg = blockCircle.parentNode.parentNode;
+  //   var path = new Path(svg, x, y);
+  // }
   getId() {
     return this.id;
   }
@@ -63,16 +63,22 @@ export default class Block {
   isSelected() {
     return this.selected;
   }
-  selectedCircle() {
+  getSelectedCircle() {
     var blockCircles = this.container.getInOutCircles();
     var selectedCircle = null;
 
     for (var circle in blockCircles) {
       if (blockCircles[circle].isSelected()) {
-        selectedCircle = circle;
+        selectedCircle = blockCircles[circle];
         break;
       }
     }
     return selectedCircle;
+  }
+  deselectCircle() {
+    if (this.getSelectedCircle() != null) {
+      var selectedCircle = this.getSelectedCircle();
+      selectedCircle.setSelected(false);
+    }
   }
 }
